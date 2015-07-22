2af09d43257a344a3c97e8de09969d220ae563e4
Tola <Tola@Tola-pc>
2015-07-22 15:49:23 +0700

    Fron End

---
+++ application/views/front/_header.php
@@ -0,0 +1,46 @@
+<div class="section section-nav">
+            <div class="container">
+                <div class="row relative">
+                    
+
+                    <div class="col-md-2 ">
+                        <div class="logo aligncenter">
+                            <div class="identity">
+                                <a href="<?php echo site_url()?>/front/" title="home" rel="home">
+                                    <img src="<?php echo base_url()?>style_front/images/downtown-logo.png" alt="Downtown theme logo">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="menu-button alignleft"></div>
+                    </div>
+                    
+
+                    <div class="col-md-5 nav-wrap">
+                        <nav class="main-nav to-right responsive-nav"> <!-- .mega-menu helper class ued as switcher -->
+                            <ul class="clean-list clearfix"> <!-- .to-right, to-left, .center-me helper class-->
+                               <li class="current-menu-item">
+                                    <a href="<?php echo site_url()?>/front/">Home</a>
+                                </li>
+                                <li class="menu-item-has-children">
+                                    <a href="<?php echo site_url()?>/front/menu">Menu</a>
+                                    <ul>
+                                        <li>
+                                            <a href="<?php echo site_url()?>/front/recipe">Recipe</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="<?php echo site_url()?>/front/booking">Reservation</a>
+                                </li>
+                                <li>
+                                    <a href="<?php echo site_url()?>/front/blog">Blog</a>
+                                </li>
+                                <li>
+                                    <a href="<?php echo site_url()?>/front/contact">Contact Us</a>
+                                </li>
+                            </ul>
+                        </nav><!-- /.main-nav -->
+                    </div>
+                </div>
+            </div>
+        </div>
