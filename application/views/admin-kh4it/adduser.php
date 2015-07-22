a397658a45ff8955be7ce8bd91ff05f19acca1b2
Chheangvuthea <chheang.vuthea@gmail.com>
2015-07-22 22:19:46 +0700

    AddUser page
    for Mr.Hehe

--- application/views/admin-kh4it/adduser.php
+++ application/views/admin-kh4it/adduser.php
@@ -1,39 +1,50 @@
+
 <!DOCTYPE html>
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-		
 		<meta name="author" content="Vuthea Chheang">
-		<title>Add Users</title>
+		<title>Add User</title>
  
-		<!-- BOOTSTRAP CSS (REQUIRED ALL PAGE)-->
-		<link href="assets/css/bootstrap.min.css" rel="stylesheet">
-		
-		<!-- PLUGINS CSS -->
-		<link href="assets/plugins/weather-icon/css/weather-icons.min.css" rel="stylesheet">
-		<link href="assets/plugins/prettify/prettify.min.css" rel="stylesheet">
-		<link href="assets/plugins/magnific-popup/magnific-popup.min.css" rel="stylesheet">
-		<link href="assets/plugins/owl-carousel/owl.carousel.min.css" rel="stylesheet">
-		<link href="assets/plugins/owl-carousel/owl.theme.min.css" rel="stylesheet">
-		<link href="assets/plugins/owl-carousel/owl.transitions.min.css" rel="stylesheet">
-		<link href="assets/plugins/chosen/chosen.min.css" rel="stylesheet">
-		<link href="assets/plugins/icheck/skins/all.css" rel="stylesheet">
-		<link href="assets/plugins/datepicker/datepicker.min.css" rel="stylesheet">
-		<link href="assets/plugins/timepicker/bootstrap-timepicker.min.css" rel="stylesheet">
-		<link href="assets/plugins/validator/bootstrapValidator.min.css" rel="stylesheet">
-		<link href="assets/plugins/summernote/summernote.min.css" rel="stylesheet">
-		<link href="assets/plugins/markdown/bootstrap-markdown.min.css" rel="stylesheet">
-		<link href="assets/plugins/datatable/css/bootstrap.datatable.min.css" rel="stylesheet">
-		<link href="assets/plugins/morris-chart/morris.min.css" rel="stylesheet">
-		<link href="assets/plugins/c3-chart/c3.min.css" rel="stylesheet">
-		<link href="assets/plugins/slider/slider.min.css" rel="stylesheet">
-		
-		<!-- MAIN CSS (REQUIRED ALL PAGE)-->
-		<link href="assets/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-		<link href="assets/css/style.css" rel="stylesheet">
-		<link href="assets/css/style-responsive.css" rel="stylesheet">
- 
+	<!-- BOOTSTRAP CSS (REQUIRED ALL PAGE)-->
+	<link href="<?php echo base_url(); ?>/public/assets/css/bootstrap.min.css" rel="stylesheet">
+
+	<!-- PLUGINS CSS -->
+	<link href="<?php echo base_url(); ?>public/assets/plugins/weather-icon/css/weather-icons.min.css"
+		rel="stylesheet">
+	<link href="<?php echo base_url(); ?>/public/assets/plugins/prettify/prettify.min.css" rel="stylesheet">
+	<link href="<?php echo base_url(); ?>/public/assets/plugins/magnific-popup/magnific-popup.min.css"
+		rel="stylesheet">
+	<link href="<?php echo base_url(); ?>/public/assets/plugins/owl-carousel/owl.carousel.min.css"
+		rel="stylesheet">
+	<link href="<?php echo base_url(); ?>/public/assets/plugins/owl-carousel/owl.theme.min.css"
+		rel="stylesheet">
+	<link href="<?php echo base_url(); ?>/public/assets/plugins/owl-carousel/owl.transitions.min.css"
+		rel="stylesheet">
+	<link href="<?php echo base_url(); ?>/public/assets/plugins/chosen/chosen.min.css" rel="stylesheet">
+	<link href="<?php echo base_url(); ?>/public/assets/plugins/icheck/skins/all.css" rel="stylesheet">
+	<link href="<?php echo base_url(); ?>/public/assets/plugins/datepicker/datepicker.min.css"
+		rel="stylesheet">
+	<link href="<?php echo base_url(); ?>/public/assets/plugins/timepicker/bootstrap-timepicker.min.css"
+		rel="stylesheet">
+	<link href="<?php echo base_url(); ?>/public/assets/plugins/validator/bootstrapValidator.min.css"
+		rel="stylesheet">
+	<link href="<?php echo base_url(); ?>/public/assets/plugins/summernote/summernote.min.css"
+		rel="stylesheet">
+	<link href="<?php echo base_url(); ?>/public/assets/plugins/markdown/bootstrap-markdown.min.css"
+		rel="stylesheet">
+	<link href="<?php echo base_url(); ?>/public/assets/plugins/datatable/css/bootstrap.datatable.min.css"
+		rel="stylesheet">
+	<link href="<?php echo base_url(); ?>/public/assets/plugins/morris-chart/morris.min.css" rel="stylesheet">
+	<link href="<?php echo base_url(); ?>/public/assets/plugins/c3-chart/c3.min.css" rel="stylesheet">
+	<link href="<?php echo base_url(); ?>/public/assets/plugins/slider/slider.min.css" rel="stylesheet">
+
+	<!-- MAIN CSS (REQUIRED ALL PAGE)-->
+	<link href="<?php echo base_url(); ?>/public/assets/plugins/font-awesome/css/font-awesome.min.css"
+		rel="stylesheet">
+	<link href="<?php echo base_url(); ?>/public/assets/css/style.css" rel="stylesheet">
+	<link href="<?php echo base_url(); ?>/public/assets/css/style-responsive.css" rel="stylesheet">
 		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
 		<!--[if lt IE 9]>
 		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
@@ -42,13 +53,11 @@
 		
 		<!--  CSS (REQUIRED ALL PAGE)-->
 		<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-		
-	
+
 	</head>
  
 	<body class="tooltips">
-		
-		
+	
 		
 		<!--
 		===========================================================
@@ -57,15 +66,18 @@
 		-->
 		<div class="wrapper">
 			<!-- BEGIN TOP NAV -->
-			<jsp:include page="_header.jsp"></jsp:include>
+			<?php $this->load->view('admin-kh4it/_header') ?>
 			<!-- END TOP NAV -->
 			
 			
 			
 			<!-- BEGIN SIDEBAR LEFT -->
-			<jsp:include page="_sidebar.jsp"></jsp:include>
+			<?php $this->load->view('admin-kh4it/_sidebar') ?>
 			<!-- END SIDEBAR LEFT -->
 			
+			
+			
+	
 			
 			
 			
@@ -74,19 +86,23 @@
 				
 				
 				<div class="container-fluid">
-					<br/>				
+					
+				<br/>				
 					<div class="panel panel-primary">
 							  <div class="panel-heading">
-								<h3 class="panel-title">Form Add User </h3>
+								<h3 class="panel-title" id="formtitle">Form Add User </h3>
 							  </div>
 							  <div class="panel-body">
-				
 					
+					
+					<div class="row">
+					
+					<div class="col-sm-12">
 					
 					<div>
-						<form id="frmadduser" id="frmadduser" method="post" action="adduser_pro.act" class="form-horizontal">
+						<form id="frmadduser" id="frmadduser" method="post" action="adduserpro" class="form-horizontal">
 							<fieldset>
-								<input type="hidden" id="userid" name="userid" >
+								
 								<div class="form-group">
 									<label class="col-lg-3 control-label">Username<span class="required">*</span></label>
 									<div class="col-lg-5">
@@ -115,7 +131,15 @@
 										<input type="password" class="form-control" name="confirmpassword" />
 									</div>
 								</div>
-							
+								<div class="form-group">
+									<label class="col-lg-3 control-label">Status<span class="required">*</span></label>
+									<div class="col-lg-5">
+										<select class="form-control" name="txtstatus">
+											<option value="1">Active</option>
+											<option value="0">DeActive</option>
+										</select>
+									</div>
+								</div>
 							
 							</fieldset>
 
@@ -127,21 +151,23 @@
 							
 						</form>
 					</div><!-- /.the-box -->
-					
-					
-				</div>
-				</div>
-					
-					
+						
+					</div><!-- /.col-sm-8 -->
+						
+				
+					</div><!-- /.row -->
+					</div>
+					</div>
 					
 				
 				</div><!-- /.container-fluid -->
 				
 				
 				
-				<!-- BEGIN FOOTER -->
-				<jsp:include page="_footer.jsp"></jsp:include>
-				<!-- END FOOTER -->
+			<!-- BEGIN FOOTER -->
+				<?php $this->load->view('admin-kh4it/_footer') ?>
+				
+			<!-- END FOOTER -->
 				
 				
 			</div><!-- /.page-content -->
@@ -164,67 +190,63 @@
 		Placed at the end of the document so the pages load faster
 		===========================================================
 		-->
-		<!-- MAIN JAVASRCIPT (REQUIRED ALL PAGE)-->
-		<script src="assets/js/jquery.min.js"></script>
-		<script src="assets/js/bootstrap.min.js"></script>
-		<script src="assets/plugins/retina/retina.min.js"></script>
-		<script src="assets/plugins/nicescroll/jquery.nicescroll.js"></script>
-		<script src="assets/plugins/slimscroll/jquery.slimscroll.min.js"></script>
-		<script src="assets/plugins/backstretch/jquery.backstretch.min.js"></script>
- 
-		<!-- PLUGINS -->
-		<script src="assets/plugins/skycons/skycons.js"></script>
-		<script src="assets/plugins/prettify/prettify.js"></script>
-		<script src="assets/plugins/magnific-popup/jquery.magnific-popup.min.js"></script>
-		<script src="assets/plugins/owl-carousel/owl.carousel.min.js"></script>
-		<script src="assets/plugins/chosen/chosen.jquery.min.js"></script>
-		<script src="assets/plugins/icheck/icheck.min.js"></script>
-		<script src="assets/plugins/datepicker/bootstrap-datepicker.js"></script>
-		<script src="assets/plugins/timepicker/bootstrap-timepicker.js"></script>
-		<script src="assets/plugins/mask/jquery.mask.min.js"></script>
-		<script src="assets/plugins/validator/bootstrapValidator.min.js"></script>
-		<script src="assets/plugins/datatable/js/jquery.dataTables.min.js"></script>
-		<script src="assets/plugins/datatable/js/bootstrap.datatable.js"></script>
-		<script src="assets/plugins/summernote/summernote.min.js"></script>
-		<script src="assets/plugins/markdown/markdown.js"></script>
-		<script src="assets/plugins/markdown/to-markdown.js"></script>
-		<script src="assets/plugins/markdown/bootstrap-markdown.js"></script>
-		<script src="assets/plugins/slider/bootstrap-slider.js"></script>
-		
-		<!-- EASY PIE CHART JS -->
-		<script src="assets/plugins/easypie-chart/easypiechart.min.js"></script>
-		<script src="assets/plugins/easypie-chart/jquery.easypiechart.min.js"></script>
-		
-		<!-- KNOB JS -->
-		<!--[if IE]>
-		<script type="text/javascript" src="assets/plugins/jquery-knob/excanvas.js"></script>
+	<!-- MAIN JAVASRCIPT (REQUIRED ALL PAGE)-->
+	<script src="<?php echo base_url(); ?>/public/assets/js/jquery.min.js"></script>
+	<script src="<?php echo base_url(); ?>/public/assets/js/bootstrap.min.js"></script>
+	<script src="<?php echo base_url(); ?>/public/assets/plugins/retina/retina.min.js"></script>
+	<script src="<?php echo base_url(); ?>/public/assets/plugins/nicescroll/jquery.nicescroll.js"></script>
+	<script src="<?php echo base_url(); ?>/public/assets/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+	<script src="<?php echo base_url(); ?>/public/assets/plugins/backstretch/jquery.backstretch.min.js"></script>
+
+	<!-- PLUGINS -->
+	<script src="<?php echo base_url(); ?>/public/assets/plugins/skycons/skycons.js"></script>
+	<script src="<?php echo base_url(); ?>/public/assets/plugins/prettify/prettify.js"></script>
+	<script
+		src="<?php echo base_url(); ?>/public/assets/plugins/magnific-popup/jquery.magnific-popup.min.js"></script>
+	<script src="<?php echo base_url(); ?>/public/assets/plugins/owl-carousel/owl.carousel.min.js"></script>
+	<script src="<?php echo base_url(); ?>/public/assets/plugins/chosen/chosen.jquery.min.js"></script>
+	<script src="<?php echo base_url(); ?>/public/assets/plugins/icheck/icheck.min.js"></script>
+	<script src="<?php echo base_url(); ?>/public/assets/plugins/datepicker/bootstrap-datepicker.js"></script>
+	<script src="<?php echo base_url(); ?>/public/assets/plugins/timepicker/bootstrap-timepicker.js"></script>
+	<script src="<?php echo base_url(); ?>/public/assets/plugins/mask/jquery.mask.min.js"></script>
+	<script src="<?php echo base_url(); ?>/public/assets/plugins/validator/bootstrapValidator.min.js"></script>
+	<script src="<?php echo base_url(); ?>/public/assets/plugins/datatable/js/jquery.dataTables.min.js"></script>
+	<script src="<?php echo base_url(); ?>/public/assets/plugins/datatable/js/bootstrap.datatable.js"></script>
+	<script src="<?php echo base_url(); ?>/public/assets/plugins/summernote/summernote.min.js"></script>
+	<script src="<?php echo base_url(); ?>/public/assets/plugins/markdown/markdown.js"></script>
+	<script src="<?php echo base_url(); ?>/public/assets/plugins/markdown/to-markdown.js"></script>
+	<script src="<?php echo base_url(); ?>/public/assets/plugins/markdown/bootstrap-markdown.js"></script>
+	<script src="<?php echo base_url(); ?>/public/assets/plugins/slider/bootstrap-slider.js"></script>
+
+	<!-- EASY PIE CHART JS -->
+	<script src="<?php echo base_url(); ?>/public/assets/plugins/easypie-chart/easypiechart.min.js"></script>
+	<script src="<?php echo base_url(); ?>/public/assets/plugins/easypie-chart/jquery.easypiechart.min.js"></script>
+
+	<!-- KNOB JS -->
+	<!--[if IE]>
+		<script type="text/javascript" src="<?php echo base_url(); ?>/public/assets/plugins/jquery-knob/excanvas.js"></script>
 		<![endif]-->
-		<script src="assets/plugins/jquery-knob/jquery.knob.js"></script>
-		<script src="assets/plugins/jquery-knob/knob.js"></script>
+	<script src="<?php echo base_url(); ?>/public/assets/plugins/jquery-knob/jquery.knob.js"></script>
+	<script src="<?php echo base_url(); ?>/public/assets/plugins/jquery-knob/knob.js"></script>
 
-		<!-- FLOT CHART JS -->
-		<script src="assets/plugins/flot-chart/jquery.flot.js"></script>
-		<script src="assets/plugins/flot-chart/jquery.flot.tooltip.js"></script>
-		<script src="assets/plugins/flot-chart/jquery.flot.resize.js"></script>
-		<script src="assets/plugins/flot-chart/jquery.flot.selection.js"></script>
-		<script src="assets/plugins/flot-chart/jquery.flot.stack.js"></script>
-		<script src="assets/plugins/flot-chart/jquery.flot.time.js"></script>
+	<!-- FLOT CHART JS -->
+	<script src="<?php echo base_url(); ?>/public/assets/plugins/flot-chart/jquery.flot.js"></script>
+	<script src="<?php echo base_url(); ?>/public/assets/plugins/flot-chart/jquery.flot.tooltip.js"></script>
+	<script src="<?php echo base_url(); ?>/public/assets/plugins/flot-chart/jquery.flot.resize.js"></script>
+	<script src="<?php echo base_url(); ?>/public/assets/plugins/flot-chart/jquery.flot.selection.js"></script>
+	<script src="<?php echo base_url(); ?>/public/assets/plugins/flot-chart/jquery.flot.stack.js"></script>
+	<script src="<?php echo base_url(); ?>/public/assets/plugins/flot-chart/jquery.flot.time.js"></script>
 
-		<!-- MORRIS JS -->
-		<script src="assets/plugins/morris-chart/raphael.min.js"></script>
-		<script src="assets/plugins/morris-chart/morris.min.js"></script>
-		
-		<!-- C3 JS -->
-		<script src="assets/plugins/c3-chart/d3.v3.min.js" charset="utf-8"></script>
-		<script src="assets/plugins/c3-chart/c3.min.js"></script>
-		
-		
-		<script src="assets/plugins/validator/example.js"></script>
-		<script src="assets/js/myvalidator.js"></script>
-		
-		<!-- MAIN APPS JS -->
-		<script src="assets/js/apps.js"></script>
-		
-		
+	<!-- MORRIS JS -->
+	<script src="<?php echo base_url(); ?>/public/assets/plugins/morris-chart/raphael.min.js"></script>
+	<script src="<?php echo base_url(); ?>/public/assets/plugins/morris-chart/morris.min.js"></script>
+
+	<!-- C3 JS -->
+	<script src="<?php echo base_url(); ?>/public/assets/plugins/c3-chart/d3.v3.min.js" charset="utf-8"></script>
+	<script src="<?php echo base_url(); ?>/public/assets/plugins/c3-chart/c3.min.js"></script>
+
+	<!-- MAIN APPS JS -->
+	<script src="<?php echo base_url(); ?>/public/assets/js/apps.js"></script>
+
 	</body>
 </html>
