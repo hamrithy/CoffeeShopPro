56e3c6531a8db277c648244a79a38961d89d1cd5
Tola <Tola@Tola-pc>
2015-07-22 15:47:58 +0700

    I edit Front  (Controller)

--- application/controllers/front.php
+++ application/controllers/front.php
@@ -3,14 +3,36 @@
 
 	class Front extends CI_Controller{
 
-		public function __construct(){
+	public function __construct(){
 			parent::__construct();
 		}
 
-		
 		public function index(){
-			
 			$this->load->view('front/index');
+		}
+		
+		public function menu(){
+			$this->load->view('front/menu');
+		}
+		
+		public function recipe(){
+			$this->load->view('front/recipe');
+		}
+		
+		public function booking(){
+			$this->load->view('front/booking');
+		}
+
+		public function blog(){
+			$this->load->view('front/blog');
+		}
+		
+		public function contact(){
+			$this->load->view('front/contact');
+		}
+		
+		public function post(){
+			$this->load->view('front/post');
 		}
 
 
