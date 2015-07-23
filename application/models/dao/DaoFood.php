94d3aa634c92342bfe9582795c22dfc3f316f72e
bunrong <bunrongleang@gmail.com>
2015-07-23 22:37:03 +0700

    i add some at dao

--- application/models/dao/DaoFood.php
+++ application/models/dao/DaoFood.php
@@ -1,1 +1,26 @@
 <?php
+class Food extends CI_Model{
+
+	public function __construct(){
+		parent::__construct();
+		$this->load->database();
+		$this->load->model('dto/DtoFood');
+	}
+
+	public function getAllFoods(){
+		$query = $this->db->get('foods');
+		return $query->result();
+	}
+
+	public function addFood(DtoFood $food){
+		$data = array('foodid' =>  $food->getFoodid(),
+					  'title'  =>	$food->getTittll()
+		);
+		return $this->db->insert('foods', $data);
+	}
+
+	public function deleteFood($id){
+		$this->db->where('foodid', $id);
+		return $this->db->delete('foods');
+	}
+}
--- application/models/dao/DaoFoodType.php
+++ application/models/dao/DaoFoodType.php
@@ -1,1 +1,32 @@
 <?php
+class FoodTypes extends CI_Model{
+
+	function __construct(){
+		parent::__construct();
+		$this->load->database();
+		$this->load->model('dto/DtoFoodType');
+
+	}
+
+	public function addFoodType(DtoFoodType $foodType){
+		$data = array('foodTypeid'	 =>		$foodType->getFoodTypeid(),
+					  'title' 		 => 	$foodType->getTitle(),
+					  'description'  =>		$foodType->getDescription()
+			);
+		return $this->db->insert('foodtypes', $foodType);
+	}
+
+	public function deleteFoodType($id){
+		$this->db->where('foodtypeid',$id);
+		return $this->db->delete('foodtypes');
+	}
+
+	public function updateFoodType(DtoFoodType $foodType){
+		
+	}
+
+	public function getAllFoodTypes(){
+		$query = $this->db->get('foodtypes');
+		return $query->result();
+	}
+}
