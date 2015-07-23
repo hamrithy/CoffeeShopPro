d6a9212b06912b6867572d0960b585ecc7738c51
vuthea <chheang.vuthea@gmail.com>
2015-07-23 11:18:18 +0700

    change side bar link

--- application/views/admin-kh4it/_sidebar.php
+++ application/views/admin-kh4it/_sidebar.php
@@ -97,7 +97,7 @@
 								  </div>
 									<div class="collapse" id="accordion-1-child-8">
 									  <div class="panel-body">
-										<i ></i>&nbsp;<a href="<?php echo site_url("admin/slide/listsslide");?>">List Slides</a><br><br>
+										<i ></i>&nbsp;<a href="<?php echo site_url("admin/slide/listslide");?>">List Slides</a><br><br>
 										<i ></i>&nbsp;<a href="<?php echo site_url("admin/slide/addslide");?>">Add Slide</a><br><br>
 										
 									  </div><!-- /.panel-body -->
