991a776f2d99aa9b53ae9965e213c31dd56b9714
tola <tola@tola-pc>
2015-07-23 20:00:45 -0700

    addpost

--- application/views/admin-kh4it/addpost.php
+++ application/views/admin-kh4it/addpost.php
@@ -83,7 +83,7 @@
 							$("#txtpostid").val("<?php echo $v->postid?>");
 							$("#txttitle").val("<?php echo $v->title?>");
 							$("#txtshortdescription").val("<?php echo $v->shortdescription?>");
-							$("#txtdescription").text("fasdf");
+							$("#txtdescription").text("<?php echo $v->longdescription ?>");
 							$("#txtfile").val("<?php echo $v->thumbnailurl?>");
 							$("#myimagedemo").fadeIn("fast").attr('src', '<?php echo $v->thumbnailurl ?>'  );
 							document.frmpost.action="<?php echo site_url();?>/admin/post/updatepost";
