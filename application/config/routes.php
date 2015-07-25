e84688419a468139cfd83f0c0ab68b32b343ac08
darapenhchet <darapenhchet@gmail.com>
2015-07-25 13:59:44 +0700

    Add Error Page 404

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,5 +51,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $route['default_controller'] = 'home';
 $route['admin'] = 'admin/auth';
-$route['404_override'] = '';
+$route['404_override'] = 'ERROR_404';
 $route['translate_uri_dashes'] = FALSE;
