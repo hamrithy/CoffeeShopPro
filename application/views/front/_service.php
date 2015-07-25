dc584cb71572ed8cec80fe3349f64af5bb3b662d
Chheangvuthea <chheang.vuthea@gmail.com>
2015-07-25 14:49:30 +0700

    front slide n service

---
+++ application/views/front/_service.php
@@ -0,0 +1,79 @@
+<section class="section section-services">
+            <div class="container">
+                <header class="entry-header aligncenter section-header section-services-header margin-top-60">
+                    <h2 class="entry-title">
+                        Services
+                    </h2>
+                    <h3 class="entry-title-desc">
+                        Visit our Cafe Shop, impeccable service will impress you
+                    </h3>   
+                </header>
+            
+                <ul class="clean-list services-list featured-items row">
+                    <li class="col-md-4 col-sm-4">
+                        <div>
+                            <figure class="ovh">
+                                <a href="<?php echo site_url()?>/front/recipe" class="font-4x grey text-dark-blue hover-blue hover-scale-120 transition-medium">
+                                    <img src="<?php echo base_url()?>public/style_front/images/services/best-cousine.jpg" alt="Best Cousine" class="" />
+                                </a>
+                            </figure>
+                            <div class="content-block background-white border-top-orange best-cousine-icon service-icon">
+                                <h4 class="entry-title pre-line padding"><a href="<?php echo site_url()?>/front/recipe">Best cousine</a></h4><hr />
+                                <div class="content padding">
+                                    <p>
+                                        A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart
+                                    </p>
+                                </div>
+                                    <div class="svg-container">
+                                        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 628 726" preserveAspectRatio="xMidYMid meet">
+                                            <polygon points="723,314 543,625.769145 183,625.769145 3,314 183,2.230855 543,2.230855 723,314" stroke-width="4"/></svg>
+                                    </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="col-md-4 col-sm-4">
+                        <div>
+                            <figure class="ovh">
+                                <a href="<?php echo site_url()?>/front/recipe" class="font-4x grey text-dark-blue hover-blue hover-scale-120 transition-medium">
+                                    <img src="<?php echo base_url()?>public/style_front/images/services/ideal-place.jpg" alt="Ideal Place" class="" />
+                                </a>
+                            </figure>
+                            <div class="content-block background-white border-top-orange ideal-place-icon service-icon">
+                                <h4 class="entry-title pre-line padding"><a href="<?php echo site_url()?>/front/recipe">Hot Lovers</a></h4><hr />
+                                <div class="content padding">
+                                    <p>
+                                        A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart
+                                    </p>
+                                </div>
+                                    <div class="svg-container">
+                                        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 628 726" preserveAspectRatio="xMidYMid meet">
+                                            <polygon points="723,314 543,625.769145 183,625.769145 3,314 183,2.230855 543,2.230855 723,314" stroke-width="4"/></svg>
+                                    </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="col-md-4 col-sm-4">
+                        <div>
+                            <figure class="ovh">
+                                <a href="<?php echo site_url()?>/front/recipe" class="font-4x grey text-dark-blue hover-blue hover-scale-120 transition-medium">
+                                    <img src="<?php echo base_url()?>public/style_front/images/services/good-rest.jpg" alt="Good Rest" />
+                                </a>
+                            </figure>
+                            <div class="content-block background-white border-top-green good-rest-icon service-icon">
+                                <h4 class="entry-title pre-line padding"><a href="<?php echo site_url()?>/front/recipe">Cold Lover</a></h4><hr />
+                                <div class="content padding">
+                                    <p>
+                                        A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart
+                                    </p>
+                                </div>
+                                    <div class="svg-container">
+                                         <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 628 726" preserveAspectRatio="xMidYMid meet">
+                                            <polygon points="723,314 543,625.769145 183,625.769145 3,314 183,2.230855 543,2.230855 723,314" stroke-width="4" /></svg>
+                                    </div>
+                            </div>
+                        </div>
+                    </li>
+                </ul><!-- /.featured-items -->
+                
+            </div>
+        </section>
---
+++ application/views/front/_slideshow.php
@@ -0,0 +1,33 @@
+<section class="section section-slider">
+                <div id="home_slider" class="home-slider">
+                    <ul class="clean-list slidesContainer">
+                        <li class="slide">
+                                <header class="aligncenter">
+                                    <h2 class="entry-title">*Welcome to Cafe Chataeu*</h2>
+                                    <h3 class="entry-title-secondary">Summer Menu</h3>
+                                    <a href="<?php echo site_url()?>/menu" class="view center-me">View<i></i></a>
+                                </header>
+                        </li>
+
+                        <li class="slide">
+                            <div class="container">
+                                <header class="aligncenter">
+                                    <h2 class="entry-title">*The Best F&B Services*</h2>
+                                    <h3 class="entry-title-secondary">Lunch Time</h3>
+                                    <a href="<?php echo site_url()?>/menu" class="view center-me">View<i></i></a>
+                                </header>
+                            </div>
+                        </li>
+
+                        <li class="slide">
+                            <div class="container">
+                                <header class="aligncenter">
+                                    <h2 class="entry-title">*Alex Food Court*</h2>
+                                    <h3 class="entry-title-secondary">Food Guide</h3>
+                                    <a href="<?php echo site_url()?>/menu" class="view center-me">View<i></i></a>
+                                </header>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+</section>
