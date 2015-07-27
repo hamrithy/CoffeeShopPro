87ca5eab7fd1316e7d1077b74397c80e842257b8
darapenhchet <darapenhchet@gmail.com>
2015-07-27 16:46:30 +0700

    Fix When Record doesn't exist..

--- application/views/front/_besthot.php
+++ application/views/front/_besthot.php
@@ -9,24 +9,28 @@
         $("#showbesthots").html(showbesthot(data));
       });
       function showbesthot(data){
-        var str="";
-        for (var i=0; i<1; i++){
-            str+= '<header class="round-offer">'+
-'                                        <div class="shape-round background-green dish-icon">'+
-'                                                <h2 class="entry-title center-me">Best Hot</h2>'+
-'                                        </div>'+
-'                                   </header>'+
-'                                   <figure>'+
-'                                        <a href="#"><img src="'+ data[i].thumbnailurl+'" alt="portfolio item"></a>'+
-'                                        <figcaption class="background-white">'+
-'                                            <h2 class="entry-title no-margin aligncenter padding-top">'+data[i].title+' </h2>'+
-'                                            <div class="price center-me background-orange half-padding">$ '+ data[i].price+'</div>'+
-'                                        </figcaption>'+
-'                                    </figure>';
-        }
+        if(data.length>0){
+          var str="";
+          for (var i=0; i<1; i++){
+              str+= '<header class="round-offer">'+
+  '                                        <div class="shape-round background-green dish-icon">'+
+  '                                                <h2 class="entry-title center-me">Best Hot</h2>'+
+  '                                        </div>'+
+  '                                   </header>'+
+  '                                   <figure>'+
+  '                                        <a href="#"><img src="'+ data[i].thumbnailurl+'" alt="portfolio item"></a>'+
+  '                                        <figcaption class="background-white">'+
+  '                                            <h2 class="entry-title no-margin aligncenter padding-top">'+data[i].title+' </h2>'+
+  '                                            <div class="price center-me background-orange half-padding">$ '+ data[i].price+'</div>'+
+  '                                        </figcaption>'+
+  '                                    </figure>';
+          }
 
-        return str;
+          return str;
+      }else{
+        return "";
       }
+    }
 
   });
 
